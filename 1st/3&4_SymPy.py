05b8bed6587f90f4dae349b79ef04a682c5bf194
department26 <dcp89957@gmail.com>
2026-04-21 23:52:02 +0530

    Add files via upload

---
+++ 1st/3&4_SymPy.py
@@ -0,0 +1,4 @@
+from sympy import symbols
+x=symbols('x')
+expression=x+5
+print(expression)
---
+++ 1st/6_matplotlib.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as div
+x=[1,2,3,4]
+y=[4,6,8,8]
+div.plot(x,y)
+div.title("A Basic Graph")
+div.xlabel("X-Axis")
+div.ylabel("Y-Axis")
+div.grid(True)
+div.show()
